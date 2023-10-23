6927583335a7ddef419008f9d140241a925bcfff
Your Name <96912707+Desenvolvimento15@users.noreply.github.com>
2023-10-23 07:25:06 -0300

    vs01

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,8 +28,6 @@ $('.galeria').slick({
           slidesToScroll: 1
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
+
     ]
   });
