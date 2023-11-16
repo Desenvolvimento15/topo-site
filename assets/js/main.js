7bab0624305d4379fd6aa071c64759a7022a0b14
Your Name <96912707+Desenvolvimento15@users.noreply.github.com>
2023-11-16 10:38:12 -0300

    vs-01

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,33 @@
 $('.galeria').slick({
-    dots: true,
+    dots: false,
+    arrows: false,
     infinite: false,
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 1000,
     responsive: [
+        
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
+          dots:  false
         }
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          infinite: true,
+          dots:  false
         }
       },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
+  
 
     ]
   });
