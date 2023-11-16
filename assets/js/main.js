0c52c62a31b0a12b259982ae5db6bb70a92e8c7f
Your Name <96912707+Desenvolvimento15@users.noreply.github.com>
2023-11-16 11:14:05 -0300

    vs01

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,8 @@
-$('.galeria').slick({
-    dots: false,
-    arrows: false,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    responsive: [
-        
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots:  false
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots:  false
-        }
-      },
-  
+$(function (){
 
-    ]
+  createGaleria({
+    duration:80000, 
+    padding:20
   });
+
+});
